1c66cf8652be2fd69b9d3791ee99852f2e298a33
vision-05 <t.frankel@outlook.com>
2026-04-11 15:41:41 +0100

    added project template

---
+++ portfolio/src/portfolio/views.cljs
@@ -0,0 +1,12 @@
+(ns portfolio.views
+  (:require
+   [re-frame.core :as re-frame]
+   [portfolio.subs :as subs]
+   ))
+
+(defn main-panel []
+  (let [name (re-frame/subscribe [::subs/name])]
+    [:div
+     [:h1
+      "Hello from " @name]
+     ]))
