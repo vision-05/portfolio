b0eb85af8ab465c56392d60d27cc8e195ada2f79
vision-05 <t.frankel@outlook.com>
2026-05-06 13:50:19 +0100

    started styling and latex

--- portfolio/src/portfolio/views.cljs
+++ portfolio/src/portfolio/views.cljs
@@ -3,18 +3,48 @@
    [re-frame.core :as re-frame]
    [portfolio.subs :as subs]
    [portfolio.events :as events]
+   [reagent.core :as r]
+   [reagent.dom :as rdom]
    [markdown.core :refer [md->html]]
    ))
 
 (defn posts-list []
   (let [posts @(re-frame/subscribe [::subs/post-slugs])]
-    (conj [:ul] (map #(vector :button {:key (:slug %) :on-click (fn [] (re-frame/dispatch [::events/fetch-post (:slug %)]))} (:slug %)) posts))))
+    (conj [:ul] (map #(vector :div.cyber-button {:key (:slug %) :on-click (fn [] (re-frame/dispatch [::events/fetch-post (:slug %)]))} (:slug %)) posts))))
 
 (defn post-view []
   (let [cur-post @(re-frame/subscribe [::subs/cur-post])
         posts @(re-frame/subscribe [::subs/posts])]
     (if (some? cur-post)
       [:div {:dangerouslySetInnerHTML {:__html (md->html (get posts cur-post))}}])))
+
+
+(defn render-math! [dom-node]
+  (when (exists? js/renderMathInElement)
+    (js/renderMathInElement 
+     dom-node
+     ;; Configure the delimiters you want to use in your markdown
+     #js {:delimiters #js [#js {:left "$$" :right "$$" :display true}
+                           #js {:left "$" :right "$" :display false}]
+          :throwOnError false})))
+
+(defn blog-post-view []
+  (r/create-class
+    {:component-did-mount
+     (fn [this] 
+       (render-math! (rdom/dom-node this)))
+        
+     :component-did-update
+     (fn [this] 
+       (render-math! (rdom/dom-node this)))
+        
+     :reagent-render
+     (fn []
+       (let [cur-post @(re-frame/subscribe [::subs/cur-post])
+             posts    @(re-frame/subscribe [::subs/posts])]
+         (if cur-post
+           [:div {:dangerouslySetInnerHTML {:__html (md->html (get posts cur-post))}}]
+           [:div "Loading post..."])))}))
 
 (defn error-view []
   (let [error @(re-frame/subscribe [::subs/error])]
@@ -27,4 +57,4 @@
      [error-view]
      [posts-list]
      [:h1 cur-post]
-     [post-view]]))
+     [blog-post-view]]))
